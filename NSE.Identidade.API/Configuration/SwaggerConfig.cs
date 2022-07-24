366e296812cab209c1343bd44cad01da93edce1e
leonardo.silva <leonardo.silva@ltm.digital>
2022-07-24 01:54:01 -0300

    primeiro commit

---
+++ NSE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "NerdStore Enterprise Identity API",
+                    Description = "Api do curso Enterprise Aplications",
+                    Contact = new OpenApiContact() { Name = "Leonardo Ribeiro", Email = "leonardo@desenvolvedor.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licence/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerconfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(swaggerUi =>
+            {
+                swaggerUi.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
---
+++ NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : Controller
+    {
+        protected ICollection<string> Erros = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+                return Ok(result);
+
+
+            var problemDetails = new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                { "Mensagens", Erros.ToArray()}
+            });
+
+            return BadRequest(problemDetails);
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+
+            foreach (var erro in erros)
+            {
+                AdicionarErroProcessamento(erro.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool OperacaoValida() => !Erros.Any();
+
+        protected void AdicionarErroProcessamento(string erro) => Erros.Add(erro);
+
+        protected void LimparErrosProcessamento() => Erros.Clear();
+    }
+}
